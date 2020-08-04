c5b90b28241a543b84d2a3998eb3eb9d2ea5ef2e
niyingying <928514062@qq.com>
2020-08-04 23:20:56 +0800

    JSX environment config

---
+++ 前端训练营/code/Component_fromweek13_2/component/webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    entry:'./main.js',
+    module:{
+        rules:[
+            {
+                test:/\.js$/,
+                use:{
+                    loader:'babel-loader',
+                    options:{
+                        presets:['@babel/preset-env'],
+                        //一个数组是一个插件, pragma意思是翻译成什么
+                        plugins:[['@babel/plugin-transform-react-jsx',{pragma:"createEle"}]]
+                    }
+                }
+            }
+        ]
+    },
+    //使得编译好的代码可读
+    mode:'development',
+    optimization:{
+        minimize:false
+    }
+
+
+}
